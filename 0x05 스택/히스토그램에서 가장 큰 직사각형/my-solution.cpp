e6881465319428ff0b905980130277ea2ce9be00
sjseo <muiruat@naver.com>
2022-03-29 05:39:54 +0900

    0x05 스택 히스토그램에서 가장 큰 직사각형까지

---
+++ 0x05 스택/히스토그램에서 가장 큰 직사각형/my-solution.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack>
+#include <utility>
+using namespace std;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n = 1;
+    while (n)
+    {
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            int m;
+            cin >> m;
+        }
+    }
+
+    return 0;
+}
