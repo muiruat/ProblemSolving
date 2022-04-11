f0450e2e0f30b2030edff70c1fe5d20a6817e1c6
sjseo <muiruat@naver.com>
2022-04-12 05:49:23 +0900

    0x0C 백트래킹 N과 M (6)까지

---
+++ 0x0C 백트래킹/N과 M (6)/my-solution.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[8];
+bool check[8];
+int ans[8];
+
+void solution(int k, int l)
+{
+    if (k == m)
+    {
+        for (int i = 0; i < m; i++)
+            cout << ans[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < l)
+            continue;
+
+        if (!check[i])
+        {
+            ans[k] = arr[i];
+            check[i] = true;
+            solution(k + 1, arr[i]);
+            check[i] = false;
+        }
+    }
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n >> m;
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    sort(arr, arr + n);
+    solution(0, 1);
+
+    return 0;
+}
